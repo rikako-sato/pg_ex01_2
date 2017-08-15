67cfaa4cfed0f5a149c76a2d51ffa8b77154af85
z2160040 <z2160040@192.168.128.100>
2017-08-16 06:08:59 +0900

    JUnit作成により修正かつJunit作成

---
+++ src/pg_ex01_2/Other.java
@@ -0,0 +1,13 @@
+package pg_ex01_2;
+
+public class Other {
+	String kekka = null;
+	public String other(){
+		kekka = "変換できません";
+		System.out.println("変換できません");
+
+		return kekka;
+
+	}
+
+}
--- src/pg_ex01_2/PG_ex01_2.java
+++ src/pg_ex01_2/PG_ex01_2.java
@@ -1,7 +1,6 @@
 
 package pg_ex01_2;
-import java.util.HashMap;
-import java.util.Map;
+
 public class PG_ex01_2 {
 
 	public static void main(String[] args) {
@@ -10,63 +9,23 @@ public class PG_ex01_2 {
 		int usernum = new java.util.Scanner(System.in).nextInt();
 
 		if(1 <= usernum && usernum <= 10){
+
+			Romenum1_10 romenum1_10 = new Romenum1_10();
 			//1～10の数字を変換するメソッド呼び出し
-			Romenum1_10(usernum);
+			romenum1_10.Romenum1_10(usernum);
+
 		}else if(11 <= usernum && usernum <= 20){
+
+			Romenum11_20 romenum11_20 = new Romenum11_20();
 			//11～20の数字を変換するメソッド呼び出し
-			Romenum11_20(usernum);
+			romenum11_20.Romenum11_20(usernum);
 
 		}else{
-			System.out.println("変換できません");
+			Other other = new Other();
+			other.other();
 		}
 
 
 	}
-	//1～10の数字を変換するメソッド
-	public static void Romenum1_10(int usernum1_10){
-
-		Map<Integer,String>translate = new HashMap<Integer,String>();
-		translate.put(1, "Ⅰ");
-		translate.put(2, "Ⅱ");
-		translate.put(3, "Ⅲ");
-		translate.put(4, "Ⅳ");
-		translate.put(5, "Ⅴ");
-		translate.put(6, "Ⅵ");
-		translate.put(7, "Ⅶ");
-		translate.put(8, "Ⅷ");
-		translate.put(9, "Ⅸ");
-		translate.put(10, "Ⅹ");
-
-			//for文で該当の数字を探す
-			for(Integer key :translate.keySet()){
-				String value = translate.get(key);
-				if(key == usernum1_10){
-				System.out.println(key + "のローマ数字は" + value);
-				}
-			}
-	}
-	//11～20の数字を変換するメソッド
-	public static void Romenum11_20(int usernum11_20){
-
-		Map<Integer,String>translate = new HashMap<Integer,String>();
-		translate.put(11, "Ⅰ");
-		translate.put(12, "Ⅱ");
-		translate.put(13, "Ⅲ");
-		translate.put(14, "Ⅳ");
-		translate.put(15, "Ⅴ");
-		translate.put(16, "Ⅵ");
-		translate.put(17, "Ⅶ");
-		translate.put(18, "Ⅷ");
-		translate.put(19, "Ⅸ");
-		translate.put(20, "Ⅹ");
-
-		//for文で該当の数字を探す
-		for(Integer key :translate.keySet()){
-			String value = translate.get(key);
-			if(key == usernum11_20){
-			System.out.println(key + "のローマ数字は" + "Ⅹ"+value);
-			}
-		}
 
 	}
-}
