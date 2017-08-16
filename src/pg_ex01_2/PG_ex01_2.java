576e8f8903e6295dbfa17ed1e07f7c6b4ec2c221
AMANO Masaru <pg2017.educator@gamil.com>
2017-08-16 17:28:33 +0900

    回答例

--- src/pg_ex01_2/PG_ex01_2.java
+++ src/pg_ex01_2/PG_ex01_2.java
@@ -1,31 +1,14 @@
-
 package pg_ex01_2;
+
+import java.util.Scanner;
 
 public class PG_ex01_2 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("1～20までの数字を入力してください");
-		int usernum = new java.util.Scanner(System.in).nextInt();
+		int usernum = new Scanner(System.in).nextInt();
 
-		if(1 <= usernum && usernum <= 10){
-
-			Romenum1_10 romenum1_10 = new Romenum1_10();
-			//1～10の数字を変換するメソッド呼び出し
-			romenum1_10.Romenum1_10(usernum);
-
-		}else if(11 <= usernum && usernum <= 20){
-
-			Romenum11_20 romenum11_20 = new Romenum11_20();
-			//11～20の数字を変換するメソッド呼び出し
-			romenum11_20.Romenum11_20(usernum);
-
-		}else{
-			Other other = new Other();
-			other.other();
-		}
-
+		System.out.println(usernum + "のローマ数字は" + Arabic.toRoman(usernum));
 
 	}
-
-	}
+}
